dd35fd63675d76bb1f6e69ad5b7d7408de95f08d
alu0100406122 <alu0100406122@ull.edu.es>
2015-10-29 18:18:54 +0000

    Modificando fallos

--- test/tc_racional.rb
+++ test/tc_racional.rb
@@ -1,23 +1,21 @@
-require '../lib/numeros'
+require '../lib/racional.rb'
 require 'test/unit'
 
 class TestPoint < Test::Unit::TestCase
   def setup
-    @origen = Point.new(0,0)
-    @unidad = Point.new(1,1)
+    @origen = Racional.new(0,0)
+    @unidad = Racional.new(1,1)
   end
   def tear_down
     # nothing
-  end
+  end 
   def test_simple
-    assert_equal("(0,0)", @origen.to_s)
-    assert_equal("(5,5)", (@unidad*5).to_s)
-    assert_equal("(-1,-1)", (-@unidad).to_s)
-    assert_equal("(1,1)", (@origen + @unidad).to_s)
+    assert_equal("(0/0)", @origen.to_s)
+    assert_equal("(5/5)", (@unidad*5).to_s)
+    assert_equal("(-1/-1)", (-@unidad).to_s)
+    assert_equal("(1/1)", (@origen + @unidad).to_s)
   end
-  def test_type_check
-    assert_raise(RuntimeError) {Point.new('1','1')}
-  end
+  
   def test_failure
     assert_equal("(5,5)", (@origen * 5).to_s, "Producto escalar")
   end
